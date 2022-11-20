61931bd490daf471687a240d768dc01a0e6fcedc
Florian Bramer <florian.bramer@esrlabs.com>
2022-11-20 19:10:28 +0100

    Added diff support for merge commits

--- src/views/diff_view.rs
+++ src/views/diff_view.rs
@@ -18,13 +18,27 @@ impl DiffView {
         }
     }
 
-    #[rustfmt::skip]
     pub fn set_commit(self: &mut Self, entry: &RepoCommit) {
         self.commit = Some(entry.clone());
 
         self.list_view = ListView::new();
-        self.list_view.insert_colorful_string(format!("Repo:       {}", entry.repo.rel_path), *RED);
+        self.list_view
+            .insert_colorful_string(format!("Repo:       {}", entry.repo.rel_path), *RED);
 
+        // we first add the output of git show without diff (does not work nicely for merge
+        // commits yet - but support will come in never versions of git-show...)
+        self.add_git_show_output(&entry);
+
+        self.list_view
+            .insert_colorful_string("―――".to_string(), *YELLOW);
+
+        // now at the diff output between the given commit and its first parent
+        // this will then also work nicely with merge commits
+        self.add_git_diff_output(&entry);
+    }
+
+    #[rustfmt::skip]
+    fn add_git_show_output(self: &mut Self, entry: &RepoCommit){
         let output = Command::new("git")
                      .current_dir(&entry.repo.abs_path)
                      .arg("--no-pager")
@@ -32,10 +46,29 @@ impl DiffView {
                      .arg("--patch-with-stat")
                      .arg("--encoding=UTF-8")
                      .arg("--pretty=fuller")
-                     .arg("--root")
+                     .arg("--no-color")
+                     .arg("--no-patch")
+                     .arg(format!("{}", entry.commit_id))
+                     .output()
+                     .expect("Failed to execute git-show command. git not installed?");
+
+        for line in String::from_utf8_lossy(&output.stdout).lines() {
+            self.list_view.insert_colorful_string(line.to_string(), Self::color_of(line));
+        }
+    }
+
+    #[rustfmt::skip]
+    fn add_git_diff_output(self: &mut Self, entry: &RepoCommit){
+        let output = Command::new("git")
+                     .current_dir(&entry.repo.abs_path)
+                     .arg("--no-pager")
+                     .arg("diff")
+                     .arg("--patch-with-stat")
+                     .arg("--encoding=UTF-8")
+                     .arg("--pretty=fuller")
                      .arg("--patch-with-stat")
                      .arg("--no-color")
-                     .arg(format!("{}", entry.commit_id))
+                     .arg(format!("{}..{}^", entry.commit_id, entry.commit_id))
                      .output()
                      .expect("Failed to execute git-show command. git not installed?");
 
